1ed3569e0a969f156ee7a44c411ca8332ebf6b5a
Enno.S <Enno.S@gmx.net>
2021-10-27 19:39:15 +0100

    end of 31. superuser added

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,3 @@
-# from _typeshed import Self
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
@@ -6,11 +5,21 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
+        if not email:
+            raise ValueError("User must have an email")
         # create user model and assign
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
         return user
 
 
